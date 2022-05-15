8b6700e573b4978d281138cfda1da768e1db8458
Mutoi01 <pngando9@gmail.com>
2022-05-15 21:30:25 +0300

    add JS score calculator function

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var score = 0;
+
+function submitAnswers(){
+   var score = 0;
+   if (document.getElementById('correct1').checked){
+     score++;
+   }
+   if (document.getElementById('correct2').checked){
+     score++;
+   }
+   if (document.getElementById('correct3').checked){
+     score++;
+   }
+   alert("Your score is " + score);
+}
