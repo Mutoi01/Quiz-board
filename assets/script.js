cfffec85f61f7865898420bcb3f593f4aeed9bd7
Mutoi01 <pngando9@gmail.com>
2022-05-16 11:27:32 +0300

    add 2nd percentage function

--- assets/script.js
+++ assets/script.js
@@ -11,8 +11,25 @@ function submitAnswers(){
    if (document.getElementById('correct3').checked){
      score++;
    }
-   alert("Your score is " + score);
+   alert("Your score is " + score );
+   alert( result(score));
 }
 
 
-  
+function result(score){
+
+
+    var percentage = score/3 * 100;
+     
+    if (percentage > 80){
+      alert("Excellent");
+    }
+    else if (percentage >50 && percentage < 80  ){
+      alert("Fair");
+    }  
+    else if (percentage< 50){
+      alert("Poor! Retake the test");
+    }
+}
+
+result()
